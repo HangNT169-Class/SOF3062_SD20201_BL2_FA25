5faac74853a4e55c000a1b882fabe78dda2efd11
HangNT169 <hangnt169@fpt.edu.vn>
2025-11-05 14:03:20 +0700

    Buổi 2: Restfull API - Phần 1 - CRUD 1 bảng

---
+++ server/src/main/java/com/poly/server/model/request/CategoryRequest.java
@@ -0,0 +1,16 @@
+package com.poly.server.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryRequest {
+
+    private Long id;
+
+    private String categoryCode;
+
+    private String categoryName;
+
+}
